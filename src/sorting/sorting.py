71eb9335187522478a93bf12fb3d20024133c80e
Tommy <tommykindle83@gmail.com>
2020-08-11 12:58:05 -0700

    abandoned my first approach refactored passing now

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,35 +1,63 @@
-def merge_sort(arr):
-    if len(arr) == 1:
-        return arr
-    middleIdx = len(arr) // 2
-    left = arr[:middleIdx]
-    right = arr[middleIdx:]
-    return merge(merge_sort(left), merge_sort(right))
-# TO-DO: complete the helper function below to merge 2 sorted arrays
+# def merge_sort(arr):
+#     if len(arr) > 1:
+#         return arr
+#     middleIdx = len(arr) // 2
+#     left = arr[:middleIdx]
+#     right = arr[middleIdx:]
+#     return merge(merge_sort(left), merge_sort(right))
+# # TO-DO: complete the helper function below to merge 2 sorted arrays
 
 
+# def merge(left, right):
+#     merged_arr = [None] * (len(left) + len(right))
+
+#     k = i = j = 0
+#     while i < len(left) and j < len(right):
+#         if left[i] <= right[j]:
+#             merged_arr[k] = left[i]
+#             i += 1
+#         else:
+#             merged_arr[k] = right[j]
+#             j += 1
+#         k += 1
+#     while i < len(left):
+#         merged_arr[k] = left[i]
+#         i += 1
+#         k += 1
+#     while j < len(right):
+#         merged_arr[k] = right[j]
+#         j += 1
+#         k += 1
+
+#     return merged_arr
 def merge(left, right):
-    merged_arr = [None] * (len(left) + len(right))
 
-    k = i = j = 0
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            merged_arr[k] = left[i]
-            i += 1
+    left_index, right_index = 0, 0
+    merged = []
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
         else:
-            merged_arr[k] = right[j]
-            j += 1
-        k += 1
-    while i < len(left):
-        merged_arr[k] = left[i]
-        i += 1
-        k += 1
-    while j < len(right):
-        merged_arr[k] = right[j]
-        j += 1
-        k += 1
+            merged.append(right[right_index])
+            right_index += 1
 
-    return merged_arr
+    merged += left[left_index:]
+    merged += right[right_index:]
+    return merged
+
+
+def merge_sort(arr):
+
+    if len(arr) <= 1:
+        return arr
+
+    middleIdx = len(arr) // 2
+    left = merge_sort(arr[:middleIdx])
+    right = merge_sort(arr[middleIdx:])
+
+    return merge(left, right)
+
 
 # TO-DO: implement the Merge Sort function below recursively
 
@@ -41,7 +69,7 @@ def merge(left, right):
 
 
 # def merge_in_place(arr, start, mid, end):
-    # Your code here
+# Your code here
 
-    # def merge_sort_in_place(arr, l, r):
-    #     pass
+# def merge_sort_in_place(arr, l, r):
+#     pass
